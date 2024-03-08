5fa23bd35941d4063d5da0cc90d0377bba785397
lorenzoArcangeli <lorenz.arcangeli@studenti.unicam.it>
2024-03-08 09:46:59 +0100

    add streamlit configuration

--- chat.py
+++ chat.py
@@ -9,9 +9,9 @@ class Chat:
         if 'history' not in st.session_state:
             st.session_state['history'] = []
         if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Ciao! Ho alcune domande"]
+            st.session_state['generated'] = ["Ciao! Ho alcune risposte (forse)"]
         if 'past' not in st.session_state:
-            st.session_state['past'] = ["Ciao! Ho alcune risposte (forse)"]
+            st.session_state['past'] = ["Ciao! Ho alcune domande"]
         # Create containers for chat history and user input
         self.response_container = st.container()
         self.container = st.container()
@@ -37,7 +37,7 @@ class Chat:
         # User input form
         with self.container:
             with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="Chiedi quello che vuoi ai documenti APRA", key='input')
+                user_input = st.text_input("Question:", placeholder="Chiedi quello che vuoi ai documenti APRA", key='input')
                 submit_button = st.form_submit_button(label='Send')
             if submit_button and user_input:
                 output = self.__conversational_chat(user_input, self.__chain)
@@ -48,7 +48,7 @@ class Chat:
         if st.session_state['generated']:
             with self.response_container:
                 for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo=('https://stan-leigh-avatar-maker-app-avatar-app-4f6sov.streamlit.app/~/+/media/43be3efab5fab4def2108eefc9ddd4998734b22e4ade3325ea9bd1eb.png'))
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo=('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5NyDxIkmwUsJeq3W3hEKBGagV5su6CE1sZQ&usqp=CAU'))
                     message(st.session_state["generated"][i], key=str(i), logo=('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzs4aNNoy8aM6p0cArrghtTo4MqOaDk5otpXuGXN1eIxpT3EHhTyLvel0c-bx15oKb1o&usqp=CAU'))
 
                     #st.write(user_template.replace(
